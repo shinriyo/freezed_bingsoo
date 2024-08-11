90adbd6a4584a9fdb5f30c07ad84def0dacb2198
shinriyo <shinriyo@gmail.com>
2024-08-11 19:43:14 +0900

    use icon for g

--- src/main/kotlin/com/shinriyo/freezed_bingsoo/CustomFileIconProvider.kt
+++ src/main/kotlin/com/shinriyo/freezed_bingsoo/CustomFileIconProvider.kt
@@ -8,13 +8,14 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Iconable
 
 class CustomFileIconProvider : FileIconProvider {
-    private val customIcon: Icon = IconLoader.getIcon("/icons/custom_icon.png", javaClass)
+    private val customIconG: Icon = IconLoader.getIcon("/icons/custom_icon_g.png", javaClass)
+    private val customIconFreezed: Icon = IconLoader.getIcon("/icons/custom_icon_freezed.png", javaClass)
 
     override fun getIcon(file: VirtualFile, @Iconable.IconFlags flags: Int, project: Project?): Icon? {
-        return if (file.name.endsWith(".g.dart") || file.name.endsWith(".freezed.dart")) {
-            customIcon
-        } else {
-            null
+        return when {
+            file.name.endsWith(".g.dart") -> customIconG
+            file.name.endsWith(".freezed.dart") -> customIconFreezed
+            else -> null
         }
     }
 }
